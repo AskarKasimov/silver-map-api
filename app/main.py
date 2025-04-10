9f174f1ac0c3c1f9d95e50035b8f0fa96f88cc55
Askar Kasimov <kasimovaskar123@gmail.com>
2025-04-11 01:43:33 +0300

    Swagger info added

--- app/main.py
+++ app/main.py
@@ -1,17 +1,49 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from database import get_connection
 from queries import *
+from typing import List
+from pydantic import BaseModel
 
-app = FastAPI()
+
+class Error(BaseModel):
+    detail: str
 
 
-@app.get("/event")
+class Event(BaseModel):
+    id: int
+    name: str
+
+
+app = FastAPI(
+    title="Silvermap API",
+    description="Основной сервис бэкэнда исторического проекта ИТМО",
+    version="1.0.0"
+)
+
+
+@app.get(
+    "/event",
+    response_model=List[Event],
+    tags=["events"],
+    summary="Все события",
+    responses={
+        200: {"description": "Список событий", "model": List[Event]},
+        404: {"description": "События не найдены", "model": Error},
+        500: {"description": "Ошибка сервера", "model": Error}
+    }
+)
 async def get_all_events():
+    """
+    Получение всех событий из базы данных.
+    """
     try:
         conn = await get_connection()
-        user = await conn.fetch(GET_ALL_EVENTS)
-        if len(user) == 0:
-            raise HTTPException(404, "No events found")
-        return dict(user)
+        events = await conn.fetch(GET_ALL_EVENTS)
+        if len(events) == 0:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="No events found"
+            )
+        return events
     finally:
         await conn.close()
